ca963d0ddc39fcf8b962eb0ef752f7c5aea539e2
Adhviklal Thoppe <67297980+UniqueRed@users.noreply.github.com>
2022-07-20 13:35:08 -0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,11 @@
+function home() {
+    document.location = "../Home/Home.html";
+}
+
+function about() {
+    document.location = "../About/About.html";
+}
+
+function math() {
+    document.location = "../Subjects/Math/Math.html";
+}
