65accb064439118f404da7ee5033411da59aab2a
Adhviklal Thoppe <67297980+UniqueRed@users.noreply.github.com>
2022-07-23 12:28:04 -0400

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,14 +2,36 @@ function home() {
     document.location = "index.html";
 }
 
-function about() {
+function team() {
     document.location = "About.html";
 }
 
+function blog() {
+    document.location = "Blog.html";
+}
+
+//Subject Buttons
 function math() {
     document.location = "Math.html";
 }
 
+//Links
 function discord() {
     location.href="https://discord.gg/sfEQkrQj";
+}
+
+function instagram() {
+    location.href="https://www.instagram.com/turtletutoringnj";
+}
+
+function linkedin() {
+    location.href="https://www.linkedin.com/company/turtletutoring";
+}
+
+function facebook() {
+    location.href="https://www.facebook.com/turtletutoringnj";
+}
+
+function twitter() {
+    location.href="https://twitter.com/turtletutnj";
 }
